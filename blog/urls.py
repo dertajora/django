475525f8e021db50b1db9fe6fe6a9b5202297d4a
Derta Isyajora Rakhman <derta.spooz@gmail.com>
2016-04-27 16:26:08 +0700

    Update Edit Post and New Post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 urlpatterns = [
 	url(r'^$',views.post_list, name='post_list'),
-	
+	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+	url(r'^post/new/$',views.post_new, name='post_new'),
+	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+	# r itu untuk mendefinisikan bahwa itu adalah regex
+	#'' ini string
+	# ^ ini untuk mendefinisikan awal dari string
+	# $ untuk mendefinisikan akhir dari string
 ]
